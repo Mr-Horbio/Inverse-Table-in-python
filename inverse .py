c02c9970b45260c66499875d89c44abfe2c0e3a4
Mr. Horbio <89149725+Mr-Horbio@users.noreply.github.com>
2022-07-08 16:30:16 -0700

    Add files via upload

---
+++ inverse .py
@@ -0,0 +1,7 @@
+# we inverse multiplication table
+No= int(input("Enter the no:"))
+count=10
+while count>=1:
+        product=No*count
+        print(No, 'x' ,count, '=' ,product)
+        count= count-1
